ff03e41892a1e367534f4af72a78de71e13164d7
tzarVOLVER <75869813+tzarVOLVER@users.noreply.github.com>
2026-05-22 17:40:08 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@ const cardsSolo   = cardsParam === "solo";
 
 const tableName = `Stage${stageNumber}Scoreboard`;
 const playerIds = [(stageNumber * 2) - 1, stageNumber * 2];
-const scoreboardApiBase = urlParams.get("apiBase") || "https://192.168.0.127:5173/api/scoreboard";
+const scoreboardApiBase = urlParams.get("apiBase") || "http://192.168.0.127:5173/api/scoreboard";
 const scoreboardApiUrl = `${scoreboardApiBase.replace(/\/$/, "")}/${stageNumber}`;
 
 export { tableName, playerIds, stageNumber, cardsHidden, cardsSolo, scoreboardApiUrl };
